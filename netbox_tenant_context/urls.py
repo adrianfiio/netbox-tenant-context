74131543b49504029d470cdf29c98d54f0ab0a9d
adrianfiio <45978851+adrianfiio@users.noreply.github.com>
2026-07-23 18:26:30 -0300

    Update urls.py

--- netbox_tenant_context/urls.py
+++ netbox_tenant_context/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from netbox_tenant_context.views import SetTenantContextView
+from netbox_tenant_context.views import SetTenantContextView, SetSiteContextView
 
 app_name = "netbox_tenant_context"
 
 urlpatterns = [
-    # Final URL: /plugins/tenant-context/set/
-    # Reverse name: plugins:netbox_tenant_context:set_context
+    # /plugins/tenant-context/set/       -> plugins:netbox_tenant_context:set_context
     path("set/", SetTenantContextView.as_view(), name="set_context"),
+    # /plugins/tenant-context/set-site/  -> plugins:netbox_tenant_context:set_site_context
+    path("set-site/", SetSiteContextView.as_view(), name="set_site_context"),
 ]
